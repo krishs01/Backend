cca510bfda0f2cbbe19dda28f2782f4432ab1985
Krish Singh 01 <krishkumarsingh2003@gmail.com>
2024-07-04 16:47:05 +0530

    first commit

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,12 @@
+import { response } from "express";
+import  asyncHandler  from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler( async (req, res) => {
+
+  response.status(200).json({
+        message: "OK"
+    })
+    return;
+})
+
+export default registerUser;
